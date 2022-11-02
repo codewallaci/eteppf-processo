94453b38f64bbe4da10211e715e1b67ae6f616f7
Wallaci Aparecido <wallaci.contato@gmail.com>
2022-11-02 09:34:57 -0300

    Finalizando o tutorial.

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -9,14 +9,19 @@ class Tutorial extends Component {
     return (
       <div id="tutorial">
         <div className="text-center bg-blue-900 flex flex-col justify-center items-center">
-          <h1 className="text-white text-2xl w-[350px] md:w-[450px] mt-12">
+          <h1 className="text-white text-1xl w-[350px] md:w-[450px] mt-12 mb-6">
             Assista o tutorial do passo a passo para realizar a inscrição no
             processo seletivo ⬇️
           </h1>
-          <div className="w-[900px] max-w-sm max-h-52 h-[350px] md:max-h-[350px] md:max-w-2xl  justify-center items-center flex rounded-md mb-24 mt-12">
-            <Video playsInline>
-              <source src={tutorialVideo} type="video/webm" />
-            </Video>
+          <div className="w-[900px] max-w-sm max-h-96 h-[350px] md:max-h-[350px] md:max-w-2xl  justify-center items-center flex rounded-md mb-24 mt-12">
+            <iframe
+              width="1349"
+              height="484"
+              src="https://www.youtube.com/embed/3CKiZUiFgBc"
+              title="INSCRIÇÃO PROCESSO SELETIVO 2023 - ETE PROFESSOR PAULO FREIRE"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen={true}
+            ></iframe>
           </div>
         </div>
       </div>
